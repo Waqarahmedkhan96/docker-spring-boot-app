7b1fd92ee6657300cd99223b2f09eea7fea2e4ac
viajook <jook@via.dk>
2026-02-21 23:29:01 +0100

    DOC1 Spring Boot Dev Container, initial commit

---
+++ src/main/java/via/doc1/docker_spring_boot_app/DockerSpringBootAppApplication.java
@@ -0,0 +1,25 @@
+package via.doc1.docker_spring_boot_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class DockerSpringBootAppApplication {
+
+	@RequestMapping("/") 
+	public String index() { 
+		return "<body bgcolor=\"#9FE2BF\">"+
+		"<h1>DOC - DevOps and Cloud</h1> " + 
+		"<h2>Dockerizing Spring Boot Application.</h2>" + 
+		"</body>"; 
+	}
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerSpringBootAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/via/doc1/docker_spring_boot_app/DockerSpringBootAppApplicationTests.java
@@ -0,0 +1,13 @@
+package via.doc1.docker_spring_boot_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerSpringBootAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
